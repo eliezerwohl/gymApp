3db342c671805d3f41fea788b35dcca18e8f9d43
eliezerwohl <eliezerwohl@gmail.com>
2016-12-16 21:57:05 -0500

    successfully did get request without ajax

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,18 @@ function sendFunc(res, string){
 }
 
 module.exports = function(app) {
+
 	app.get("/home", function(req, res){
 		sendFunc(res, "page");
 	});
+
+	app.get("/test", function(req, res){
+		res.send("you did a post request")
+	})
 		app.get("/", function(req, res){
 		sendFunc(res, "home");
 	});
+
 		app.get("*", function(req,res){
 			res.send("404'd!")
 		})
