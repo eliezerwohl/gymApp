d6095144f1af55ef397f6214bc93513319cf6712
eliezerwohl <eliezerwohl@gmail.com>
2016-12-17 12:08:52 -0500

    seperated post request to it's own js file, can use it and use a callback to process the data it sends back

--- public/assets/javascript/home.js
+++ public/assets/javascript/home.js
@@ -1,27 +1,15 @@
-function loadXMLDoc() {
-  var object1 = {
-    "we":"are",
-    "showRoom":"dummies"
-  }
-    var xmlhttp = new XMLHttpRequest();
-
-    xmlhttp.onreadystatechange = function() {
-        if (xmlhttp.readyState == XMLHttpRequest.DONE ) {
-           if (xmlhttp.status == 200) {
-               console.log(xmlhttp.responseText);
-           }
-           else if (xmlhttp.status == 400) {
-              alert('There was an error 400');
-           }
-           else {
-               alert('something else other than 200 was returned');
-           }
-        }
-    };
-
-    xmlhttp.open("POST", "posttest", true);
-    xmlhttp.setRequestHeader("Content-type", "application/json");
-    xmlhttp.send(JSON.stringify(object1));
+data = {
+  we:"are",
+  v:"r"
 }
 
-loadXMLDoc()
+request("POST","posttest",data, function (data) {
+  console.log(data)
+});
+
+
+// while(test != undefined){
+//   debugger
+//   console.log(test)
+// }
+
